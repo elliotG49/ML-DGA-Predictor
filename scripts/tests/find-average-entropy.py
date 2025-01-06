acd42e6e421ea41dc8a98d5417f7d54ae0c96d11
elliotg <elliot@elliotg.co.uk>
2025-01-06 18:05:06 +0000

    added new dataset with updated entropy values

--- scripts/tests/find-average-entropy.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-input_file = '/root/project-mitnick/datasets/domains_cleaned_with_entropy.csv'
-df = pd.read_csv(input_file, dtype={'entropy': 'float', 'isDGA': 'str'})
-average_entropy = df.groupby('isDGA')['entropy'].mean()
-print(average_entropy)
